399cd05b8e7638fadf52e73eb011877c8d3176c6
ConDavison1 <152654015+ConDavison1@users.noreply.github.com>
2024-02-18 20:14:29 -0700

    Add files via upload

---
+++ Lab5/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+//Creating a Circe class
+public class Circle
+{
+    //setting the radius attribute to double.
+    private double radius;
+
+    public Circle(double radius)
+    {
+        SetRadius(radius);
+    }
+    //Setting radius
+    public void SetRadius(double radius)
+    {
+        //cecking if radius is > than 0
+        if (radius > 0)
+        {
+            this.radius = radius;
+        }
+        //if not > than 0 throw an exception
+        else
+        {
+            throw new InvalidRadiusException(radius);
+        }
+    }
+    //creating a Tostring method displaying the circles radius infomation
+    public override string ToString()
+    {
+        return $"Circle's radius is: {radius}";
+    }
+}
+
+// Exception class handling invalid radius
+public class InvalidRadiusException : Exception
+{
+    public double Invalid { get; }
+
+    // Default Constructor
+    public InvalidRadiusException() : base("Invalid radius. Radius must be greater than zero.")
+    {
+        Invalid = 0;
+    }
+
+    // Generating an error message
+    public InvalidRadiusException(double radius) : base($"Invalid radius: {radius}. Radius must be greater than zero.")
+    {
+        Invalid = radius;
+    }   
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        //main class to test code 
+        static void Main()
+        {
+            try
+            {
+                //Working Radius because its > than 0
+                Circle circle1 = new Circle(3);
+                Console.WriteLine(circle1);
+                //Exception 1
+                Circle circle2 = new Circle(-12);
+                Console.WriteLine(circle2);
+                //Exception 2
+                Circle circle3 = new Circle(0);
+                Console.WriteLine(circle3);
+            }
+            catch (InvalidRadiusException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
